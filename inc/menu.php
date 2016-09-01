c3cd661ce96fb02880b5eaa21384c28197b46708
eliio <stevensoehl@gmail.com>
2016-09-01 07:09:36 -0400

    Delete menu.php

--- inc/menu.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-add_action('admin_menu', 'wp2x2crm_add_admin');
-
-function wp2x2crm_add_admin() {
-
-add_submenu_page( 'options-general.php' , 'x2CRM Options', 'x2CRM Options' , 'manage_options' , __FILE__ . '_display_contents' , 'wp2x2crm_display_contents');
-}
-?>
